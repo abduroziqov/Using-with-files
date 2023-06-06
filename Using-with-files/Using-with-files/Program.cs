6eb6e67484bdcd0dd3de84122a53faabde814ad8
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-06-06 10:36:23 +0500

    Create a file

---
+++ Using-with-files/Using-with-files/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Using_with_files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string file = @"C:\Users\abdur\OneDrive\Рабочий стол\PDP\3-module\Test-files\.gitignore1";
+
+            if (File.Exists(file)) // checks file (is file have)
+            {
+                Console.WriteLine("This file is already available in this folder.");
+            }
+            else
+            {
+                File.Create(file);  // creates
+                Console.WriteLine("File has been succesfully created.");
+            }
+        }
+    }
+}
