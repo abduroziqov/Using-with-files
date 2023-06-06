58167dff104bcdb70d18d158bbf2dc9734112d66
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-06-06 23:15:31 +0500

    find bin files

--- Using-with-files/Using-with-files/Program.cs
+++ Using-with-files/Using-with-files/Program.cs
@@ -1,23 +1,16 @@
 ﻿namespace Using_with_files
+namespace Using_with_files
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Give file location
-            string filePath = @"C:\Users\abdur\OneDrive\Рабочий стол\PDP\3-module\Lesson_06\test.txt";
+            string[] binFile = Directory.GetDirectories(@"C:\Users\abdur\OneDrive\Рабочий стол\PDP\3-module\Lesson_06\", "bin*");
 
-            // Create a new instance of StreamReader and pass the file path
-            using (StreamReader reader = new StreamReader(filePath))
-            {
                 string line;
 
-                // Read the file line by line until the end of the file is reached
-                while ((line = reader.ReadLine()) != null)
-                {
-                    // Process the line as needed
-                    Console.WriteLine(line);
                 }
+            // Print the file paths to the console
             }
 
             using (StreamWriter writer = new StreamWriter(filePath))
