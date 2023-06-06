9ec47987a747a3e5d0fb48e1cb5ff5d27933598e
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-06-06 23:27:09 +0500

    outputs file's full name

--- Using-with-files/Using-with-files/Program.cs
+++ Using-with-files/Using-with-files/Program.cs
@@ -1,32 +1,19 @@
-﻿namespace Using_with_files
+﻿using System.IO;
+using System.Diagnostics;
+
+namespace Using_with_files
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Give file location
-            string filePath = @"C:\Users\abdur\OneDrive\Рабочий стол\PDP\3-module\Lesson_06\test.txt";
-
-            // Create a new instance of StreamReader and pass the file path
-            using (StreamReader reader = new StreamReader(filePath))
+            string folder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            string filter = "*.exe";
+            string[] files = Directory.GetFiles(folder, filter);
+            foreach (var file in files)
             {
-                string line;
-
-                // Read the file line by line until the end of the file is reached
-                while ((line = reader.ReadLine()) != null)
-                {
-                    // Process the line as needed
-                    Console.WriteLine(line);
-                }
+                Console.WriteLine($"Name of file: {file}");
             }
-
-            using (StreamWriter writer = new StreamWriter(filePath))
-            {
-                // Write content to the file
-                writer.WriteLine("Hello, my name is Quvonchbek!");
-                writer.WriteLine("This is a sample file.");
-            }
-            Console.WriteLine("File has been written.");
         }
     }
 }
